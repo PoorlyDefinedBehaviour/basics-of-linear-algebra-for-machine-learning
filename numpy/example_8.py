68efdc2b99a44e6dd6bafeb6f43d01f52ea7bd7e
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-07-09 20:22:40 -0300

    two dimensional list to numpy.ndarray

---
+++ numpy/example_8.py
@@ -0,0 +1,17 @@
+import numpy
+
+data = numpy.array([
+    [11, 22],
+    [33, 44],
+    [55, 66]
+])
+
+# [
+#   [11, 22],
+#   [33, 44],
+#   [55, 66],
+# ]
+print(data)
+
+# <class 'numpy.ndarray'>
+print(type(data))
