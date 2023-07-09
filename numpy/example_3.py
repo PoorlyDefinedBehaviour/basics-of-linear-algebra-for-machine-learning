04b229acddf02294cca28751c1ca5dcb12d68cb6
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-07-09 20:08:35 -0300

    numpy.zeros

---
+++ numpy/example_3.py
@@ -0,0 +1,10 @@
+import numpy
+
+# zeros() creates a new ndarray with the specified size where each value is set to 0.
+
+a = numpy.zeros([3, 5])
+
+# [[0. 0. 0. 0. 0.]
+#  [0. 0. 0. 0. 0.]
+#  [0. 0. 0. 0. 0.]]
+print(a)
