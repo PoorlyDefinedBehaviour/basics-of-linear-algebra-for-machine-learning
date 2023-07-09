cd4bd3425a75481a60c10207ca841b56ef695613
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-07-09 20:09:50 -0300

    numpy.ones

---
+++ numpy/example_4.py
@@ -0,0 +1,9 @@
+import numpy
+
+# ones() creates a new ndarray with the specified size where each value is set to 1.
+
+# Create a ndarray with a row and 5 columns.
+a = numpy.ones([5])
+
+# [1. 1. 1. 1. 1.]
+print(a)
