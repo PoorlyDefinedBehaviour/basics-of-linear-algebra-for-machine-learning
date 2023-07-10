510167c8684fb77d39dd6c4bcbaa247a21e9e146
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-07-09 23:10:40 -0300

    1d + 2d ndarray broadcasting

---
+++ numpy/example_19.py
@@ -0,0 +1,20 @@
+import numpy
+
+a = numpy.array([
+    [1, 2, 3],
+    [1, 2, 3]
+])
+
+print(a)
+
+b = numpy.array([1, 2, 3])
+
+print(b)
+
+c = a + b
+
+# [
+#   [2, 4, 6],
+#   [2, 4, 6]
+# ]
+print(c)
